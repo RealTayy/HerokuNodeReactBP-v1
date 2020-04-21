b4036c126334e07a50d2244ab6b1fed782c36ddf
The Mai <mai.the.09@gmail.com>
2020-04-21 03:32:33 -0500

    Fix pathing

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ console.log("NODE_ENV:", process.env.NODE_ENV);
 |*****************/
 // Setup app to serve static files from React App depending on production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client', 'build')));
+  app.use(express.static(path.join(__dirname, '../../client/build')));
 }
 
 app.get('/api/test', (req, res) => {
