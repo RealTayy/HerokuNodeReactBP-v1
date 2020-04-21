df057ef849588f70549b6042163f2654d4857c15
The Mai <mai.the.09@gmail.com>
2020-04-21 03:44:49 -0500

    Futher refactor html routes

--- server/server.js
+++ server/server.js
@@ -11,19 +11,14 @@ console.log("NODE_ENV:", process.env.NODE_ENV);
 /*****************|
 |* SET UP ROUTES *| 
 |*****************/
-// Setup app to serve static files from React App depending on production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../client/build')));
-}
-
 app.get('/api/test', (req, res) => {
   const { TEST, NODE_ENV } = process.env;
   res.send(`${TEST} | ${NODE_ENV}`);
 })
 
-app.get('*', (req, res) => {  
-  res.sendFile("index.html");
-})
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.get('*', (req, res) => res.sendFile("index.html"));
+}
 
 app.listen(PORT, () => { console.log(`App listening on PORT: ${PORT}`) });
