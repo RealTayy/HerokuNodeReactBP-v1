1524ae8a7c619b561024442965b83a24611cd9ab
The Mai <mai.the.09@gmail.com>
2020-04-22 17:09:52 -0500

    Add mobX, CRA, react-app-rewired to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { observable, action } from "mobx";
+import { observer } from "mobx-react";
 
-function App() {
+class MobXState {
+  @observable timer = 0;
+
+  constructor() {
+    setInterval(() => {
+      this.timer += 1;
+    }, 1000);
+  }
+
+  @action.bound
+  reset() {
+    this.timer = 0;
+  }
+}
+
+const TimerView = observer(({ mobXState }) => (
+  <div>
+    <button onClick={mobXState.reset}>Seconds passed: {mobXState.timer}</button>
+  </div>
+));
+
+const App = () => {
   return (
     <div className="App">
       Hello World!
+      <TimerView mobXState={new MobXState()} />
     </div>
   );
 }
