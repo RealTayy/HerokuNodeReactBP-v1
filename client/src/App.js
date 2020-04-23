5c66b31bde4434626b73bfa04524a0735db4ad0c
The Mai <mai.the.09@gmail.com>
2020-04-23 02:25:58 -0500

    Remove commented out MobX code. Renable image import example

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,11 @@
 import React from 'react';
-import { observable, computed, action, autorun } from "mobx";
-import { observer } from 'mobx-react';
 import placeholderImage from "./assets/images/placeholder.png";
 import Timer from './components/Timer';
 
-// class TimerStoreTwo {
-//   @observable secondsPassed = 0;
-
-//   constructor() {
-//     setInterval(() => {
-//       this.secondsPassed += 1;
-//     }, 1000);
-//   }
-
-//   @action.bound
-//   reset() {
-//     this.secondsPassed = 0;
-//   }
-
-// }
-
-// const TimerView = observer(({ store }) => (
-//   <div>
-//     <button onClick={store.reset}>Seconds passed: {store.secondsPassed}</button>
-//   </div>
-// ));
-
 const App = () => {
   return (
-    <div className="App">
-      {/* <TimerView store={new TimerStoreTwo()} /> */}
-      {/* <img src={placeholderImage} alt="placeholder" /> */}
+    <div className="App">      
+      <img src={placeholderImage} alt="placeholder" />
       <Timer />
     </div>
   );
